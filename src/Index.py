a4778531a717b5f62e956ce6f45e7d7be43cd55f
DarkStarUWU <guevara4536@hotmail.com>
2022-06-23 17:29:21 -0500

    Funciona OptionMenu

--- src/Index.py
+++ src/Index.py
@@ -6,10 +6,11 @@ import psycopg2
 
  
 class Ventana():
-    
+
+
     def __init__(self, windows):
 
-        ArrayComercial = ['Vanesa Castaño', 'Jorge Calvo', 'Aniket Kanurkar', 'Gengis Rovi']
+        ArrayComercial = ['Vanesa Castano', 'Jorge Calvo', 'Aniket Kanurkar', 'Gengis Rovi']
         ArrayAsistentes = ['Jessica Prato', 'Juan Perez']
 
         self.wind = windows
@@ -53,16 +54,21 @@ class Ventana():
         self.entryFecha.bind("<Key>", self.FechaEscriba)
         self.entryFecha.bind("<BackSpace>", lambda :self.entryFecha(self.root.END))
         
+        
         self.var1 = StringVar(self.frame)
-        self.opcion1 = OptionMenu(self.frame, self.var1, *ArrayComercial, command=self.getOpcion1)
+        self.var1.set("Elegir Comercial")
+        self.opcion1 = OptionMenu(self.frame, self.var1, *ArrayComercial)
         self.opcion1.config(width=20)
         self.opcion1.place(x=200, y=240)
         
         
         self.var2 = StringVar(self.frame)
-        self.opcion2 = OptionMenu(self.frame, self.var2, *ArrayAsistentes, command=self.getOpcion2)
+        self.var2.set("Elegir Asisitente")
+        self.opcion2 = OptionMenu(self.frame, self.var2, *ArrayAsistentes)
         self.opcion2.config(width=20)
         self.opcion2.place(x=200, y=310)
+        
+        
         
         
         self.botton_ComproPago = Button(self.frame, text="Adjuntar", command=self.AbrirArchivo)
@@ -71,16 +77,8 @@ class Ventana():
         self.botton_ComproProove = Button(self.frame, text="Adjuntar", command=self.AbrirArchivo)
         self.botton_ComproProove.place(x=200, y=450)
         
-        self.botton_Guardar = Button(self.frame, text="Guardar", command=lambda:self.GuardarFormulario(
-            self.entry_NumeroPresu.get(),
-            self.entry_Cliente.get(),
-            self.entryFecha.get(),
-            self.opcion1,
-            self.opcion2,
-            self.botton_ComproPago.cget('command'),
-            self.botton_ComproProove.cget('command')
-            
-            ))
+        
+        self.botton_Guardar = Button(self.frame, text="Guardar", command=lambda:self.GuardarFormulario())
         self.botton_Guardar.place(x=200, y=700)
         
     def FechaEscriba(self, event):
@@ -90,10 +88,10 @@ class Ventana():
             for i in texto:
                 letras += 1
             
-            if letras == 2:
-                self.entryFecha.insert(2, "/")
-            elif letras == 5:
-                self.entryFecha.insert(5, "/")
+            if letras == 4:
+                self.entryFecha.insert(4, "-")
+            elif letras == 7:
+                self.entryFecha.insert(7, "-")
             
             if letras == 10:
                 self.entryFecha.insert(10, " ")
@@ -110,28 +108,37 @@ class Ventana():
         archivo = filedialog.askopenfilename(title="abrir", initialdir="C:/", filetypes=[["Archivos de Imagen", "*.jpg"]])
         print(archivo)
         
-    def GuardarFormulario(self, Numero_Pres, Cliente, Fecha_Soli, Comercial, Asistente, Compro_Pago, Compro_Proveedor):
-        conn = psycopg2.connect(dbname="postgres",
-                         user="postgres", 
-                         password="SuPer4536", 
-                         host="localhost"
-                         )
-        cursor = conn.cursor()
-        query =  "INSERT INTO Formulario(NumeroPresu,Cliente,Fecha_Solic,Comercial,Asistente,Compro_Pago,Compro_Proove) VALUES (%s, %s, %s, %s, %s, %s, %s)",
-        info =  Numero_Pres,Cliente,Fecha_Soli,Comercial,Asistente,Compro_Pago,Compro_Proveedor
-        cursor.execute(query, info)
-        print("save")
-        conn.commit()
-        conn.close()
+    def GuardarFormulario(self,*args):
+        try:
+            conn = psycopg2.connect(dbname="postgres",
+                            user="postgres", 
+                            password="SuPer4536", 
+                            host="localhost"
+                            )
+            print("Connection Successful!")
+        except: 
+            print("Unable to connect to database")
+        try:
+            
+            cursor = conn.cursor()
+            query =  "INSERT INTO Formulario(NumeroPresu,Cliente,Fecha_Solic,Comercial,Asistente, Compro_Pago, Compro_Proove) VALUES (%s, %s, %s, %s, %s,%s, %s)"
+            info =  self.entry_NumeroPresu.get(),self.entry_Cliente.get(),self.entryFecha.get(), self.var1.get(), self.var2.get(), self.botton_ComproPago.get(), self.botton_ComproProove.get()
+            cursor.execute(query, info)
+            print("save")
+            conn.commit()
+            conn.close()
+            
+        except (Exception, psycopg2.Error) as error:
+             print("Failed to insert record", error)
         
-    def getOpcion1(self):
-        global getOpcion1
-        getOpcion1 = self.var1
+    def getOpcion1(self, getOpcion1):
+        getOpcion1 = self.var1.get()
+        return getOpcion1
+   
         
-    def getOpcion2(self):
-        global getOpcion2
-        getOpcion2 = self.var2
-        
+    def getOpcion2(self, getOpcion2):
+        getOpcion2 = self.var2.get()
+        return getOpcion2
 
     
 def main():
